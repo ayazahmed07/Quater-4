e9c74e345e9ab87ed48d36e485304208997ecb90
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2026-01-17 19:38:50 +0500

    updated

--- 03 Docker/02 docker-image/main.py
+++ 03 Docker/02 docker-image/main.py
@@ -2,4 +2,9 @@ import time
 from datetime import datetime
 
 while True:
-    with open
+    now = datetime.now()
+    formatted_date = now.strftime('%d-%m-%Y')
+    with open('data.txt', 'a') as file:
+        file.write(f'Data written at: {formatted_date}\n')
+    time.sleep(5)  # Wait for 5 seconds before the next entry
+    print(f'Log entry added. {formatted_date}\n')
